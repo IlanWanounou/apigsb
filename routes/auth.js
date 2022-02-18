f9695583b9bed0ee311c383e93c43679aff9544c
Ilan Wanounou <wanounouilan@gmail.com>
2022-02-18 15:17:58 +0100

    POST -> GET

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,6 @@ const express = require('express');
 const authCrl = require('../controllers/auth');
 
 const router = express.Router();
-router.post('/login', authCrl.login);
+router.get('/login', authCrl.login);
 
 module.exports = router;
