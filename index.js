bb87eced0125ba8305e46136d5c09465e01b7f67
Ilan Wanounou <wanounouilan@gmail.com>
2022-02-26 21:39:56 +0100

    création route fichefrais

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const account = require('./routes/account');
 const auth = require("./routes/auth");
+const fichefrais = require('./routes/fichefrais')
 app.use(express.json());
 
 
@@ -9,5 +9,5 @@ app.listen(3000, () => {
     console.log('On');
 })
 
-app.use('/auth', auth)
-
+app.use('/auth', auth);
+app.use('/fichefrais', fichefrais);
