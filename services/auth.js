5a73fcf6b9b346f099d720c003a7c11854f9d87f
Ilan Wanounou <wanounouilan@gmail.com>
2022-02-26 21:42:01 +0100

    récupération de id visiteur via le token

--- services/auth.js
+++ services/auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken')
 require('dotenv').config();
+const Cookies = require( "cookies" );
+
 exports.generateToken = (user) => {
     return jwt.sign(
         {
@@ -12,3 +14,10 @@ exports.generateToken = (user) => {
         },
     );
 }
+
+exports.getIdVisteur = (req, res) => {
+    let cookie = new Cookies(req, res).get('token');
+    const regex = /"/ig;
+    cookie = cookie.replace(regex, '');
+    const id = jwt.verify(cookie, process.env.TOKEN_KEY);
+    return id.userId;
