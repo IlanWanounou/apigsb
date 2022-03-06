5022b898314b452fb1899d84fd6035cb49dced9e
Ilan Wanounou <wanounouilan@gmail.com>
2022-03-06 16:52:46 +0100

    Route frais repas

---
+++ controllers/fraisRepas.js
@@ -0,0 +1,29 @@
+const fraisRepasService = require('../services/fraisRepas')
+const {getIdVisteur} = require('../services/auth');
+
+
+exports.findOne = async (req, res) => {
+    try {
+        let data = await fraisRepasService.findOne(req.params.mois, getIdVisteur(req, res));
+        if (data) {
+            res.status(200).send(data);
+        } else {
+            res.status(200).json({message:"La fiche pour le mois demander n'exister pas", quantite: 0});
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+exports.update = async (req, res) => {
+    try {
+        let data = await fraisRepasService.update(req.params.mois, getIdVisteur(req, res), req.body.quantite);
+        if (data[0]===1) {
+            res.status(201).json({message:"modification effetuer"});
+        } else {
+            res.status(200).json({message:"La fiche pour le mois demander n'exister pas"});
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
--- routes/fichefrais.js
+++ routes/fichefrais.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const fraisKmCrl = require('../controllers/fraisKm');
+const fraisRepas = require('../controllers/fraisRepas');
+
 
 
 //fraiskm
 router.post('/fraiskm/:mois', fraisKmCrl.findOne);
 router.put('/fraiskm/:mois', fraisKmCrl.update);
+
+// fraisRepas
+router.post('/fraisrepas/:mois', fraisRepas.findOne);
+router.put('/fraisrepas/:mois', fraisRepas.update);
+
+/*// fraisNuit
+router.post('/fraisnuit/:mois', fraisNuit.findOne);
+//router.put('/fraisnuit/:mois', fraisNuit.update);
+
+// fraisetapes
+//router.post('/fraisetapes/:mois', fraisetapes.findOne);
+//router.put('/fraisetapes/:mois', fraisNuit.update);
+
+// fraisHF
+//router.post('/fraiskm/:mois', fraisHF.findOne);
+//router.put('/fraiskm/:mois', fraisHF.update);
+
+*/
+
+
+//liste fraisHF
+
+
+
 
 
 
